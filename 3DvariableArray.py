4507f5435a1230c9be7a5668d71a7cae6c608611
Akash Manna <akashmanna@gmail.com>
2016-03-24 12:25:44 +0530

    MoGraph Cloner Blender
    Removed unnecessary bits

--- 3DvariableArray.py
+++ 3DvariableArray.py
@@ -1,26 +1,17 @@
 bl_info = {
-    "name": "Cursor Array",
+    "name": "3D Cloner Array",
     "category": "Object",
 }
 
 import bpy
 from bpy import *
 
-
-class ObjectCursorArray(bpy.types.Operator):
-    """Object Cursor Array"""
-    bl_idname = "object.cursor_array"
-    bl_label = "CUBE Array"
+class VarArray(bpy.types.Operator):
+    bl_idname = "object.mograph_array"
+    bl_label = "Creaate Array"
     bl_options = {'REGISTER', 'UNDO'}
     
     scn = context.window_manager
-    
-    ''' IDK WHY BUT THIS WORKS ONLY IF YOU UNCOMMENT THE # below and comment the code of total below that, run it like that. And then undo the steps and run.   '''
-    #total = bpy.props.IntVectorProperty(name="Steps")
-    #total = bpy.context.window_manager.num
-    #total_x = total[0]
-    #total_y = total[1]
-    #total_z = total[2]
 
     def execute(self, context):
         scene = context.scene
@@ -39,7 +30,7 @@ class ObjectCursorArray(bpy.types.Operator):
                     
                     obj_new = obj.copy()
                     scene.objects.link(obj_new)
-                    #obj_new.location = (obj.location * factor) + (cursor * (1.0 - factor))
+                    
                     obj_new.location = obj.location 
                     obj_new.location.x += context.window_manager.Space[0]*factor_x
                     obj_new.location.y += context.window_manager.Space[1]*factor_y
@@ -48,8 +39,8 @@ class ObjectCursorArray(bpy.types.Operator):
         return {'FINISHED'}
     
     
-class HelloWorldPanel(bpy.types.Panel):
-    bl_label = "MoGraph: 3DCUBE Array Cloner"
+class ArrayPanel(bpy.types.Panel):
+    bl_label = "MoGraph: 3D Array Cloner"
     bl_idname = "OBJECT_PT_hello"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
@@ -64,36 +55,28 @@ class HelloWorldPanel(bpy.types.Panel):
         
         row = layout.row()
         row.prop(scn, "num")
- 
         
         row=layout.row()
         row.prop(scn, "Space")
         
         row = layout.row()
-        row.operator("object.cursor_array")
-
-
+        row.operator("object.mograph_array")
 
 def menu_func(self, context):
     self.layout.operator(ObjectCursorArray.bl_idname)
 
-
 def register():
-    bpy.utils.register_class(ObjectCursorArray)
-    bpy.utils.register_class(HelloWorldPanel)
+    bpy.utils.register_class(VarArray)
+    bpy.utils.register_class(ArrayPanel)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
-
 def unregister():
-  
-
-    bpy.utils.unregister_class(ObjectCursorArray)
+    bpy.utils.unregister_class(VarArray)
     bpy.types.VIEW3D_MT_object.remove(menu_func)
-    bpy.utils.unregister_class(HelloWorldPanel)
+    bpy.utils.unregister_class(ArrayPanel)
 
 if __name__ == "__main__":
-    bpy.types.WindowManager.num = bpy.props.IntVectorProperty(name="EDGE:", description = "Number of Cloner Objects to be created on an edge")
-   
+    bpy.types.WindowManager.num = bpy.props.IntVectorProperty(name="EDGE:", description = "Number of Cloner Objects to be created on an edge", default = (1,1,1))
     bpy.types.WindowManager.Space = bpy.props.FloatVectorProperty(name="Spacing")
 
     register()
